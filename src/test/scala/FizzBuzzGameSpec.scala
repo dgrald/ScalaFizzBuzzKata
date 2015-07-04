565604888accbf303725e6b0b58b41a0be509173
dgrald <grald.dylan@gmail.com>
2015-07-04 14:29:49 -0700

    updated mocking in FizzBuzzGameSpec to be a bit simpler

--- src/test/scala/FizzBuzzGameSpec.scala
+++ src/test/scala/FizzBuzzGameSpec.scala
@@ -3,6 +3,7 @@ import java.io.Writer
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{Matchers, FlatSpec}
 import org.mockito.Mockito._
+import org.mockito.Matchers.anyInt
 
 /**
  * Created by dylangrald on 7/3/15.
@@ -11,8 +12,10 @@ class FizzBuzzGameSpec extends FlatSpec with Matchers with MockitoSugar {
 
   "The game" should "write the FizzBuzz values to the writer" in {
     val fizzBuzzCalculatorMock = mock[FizzBuzzCalculator]
-    (1 to 100).foreach(num => when(fizzBuzzCalculatorMock.fizzBuzzValue(num)).thenReturn("Fizz"))
+    when(fizzBuzzCalculatorMock.fizzBuzzValue(anyInt())).thenReturn("Fizz")
+
     val writerMock = mock[Writer]
+
     val fizzBuzzGame = FizzBuzzGame(fizzBuzzCalculatorMock, writerMock)
     fizzBuzzGame.playFizzBuzz(1, 100)
 
